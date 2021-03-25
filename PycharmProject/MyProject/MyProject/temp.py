454e78472718b3f698e906d08678a1aa04c2bd25
Keya12 <bhavsarkeya@gmail.com>
2021-03-25 09:30:33 +0530

    initial

---
+++ PycharmProject/MyProject/MyProject/temp.py
@@ -0,0 +1,128 @@
+import numpy as np
+import cv2
+from collections import deque
+import time
+def setValues(x):
+   print("")
+
+# Giving different arrays to handle colour points of different colours
+bpoints = [deque(maxlen=1024)]
+gpoints = [deque(maxlen=1024)]
+rpoints = [deque(maxlen=1024)]
+ypoints = [deque(maxlen=1024)]
+#assigning index values
+blue_index = 0
+green_index = 0
+red_index = 0
+yellow_index = 0
+kernel = np.ones((5,5),np.uint8)
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+colorIndex = 0
+
+#starting the painting window setup
+paintWindow = np.zeros((471,636,3)) + 255
+cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    #Flipping the frame just for convenience
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    u_hue = 153
+    u_saturation = 255
+    u_value = 255
+    l_hue = 64
+    l_saturation = 72
+    l_value = 49
+    
+    Upper_hsv = np.array([u_hue,u_saturation,u_value])
+    Lower_hsv = np.array([l_hue,l_saturation,l_value])
+    frame = cv2.rectangle(frame, (40,1), (140,65), (122,122,122), -1)
+    frame = cv2.rectangle(frame, (160,1), (255,65), colors[0], -1)
+    frame = cv2.rectangle(frame, (275,1), (370,65), colors[1], -1)
+    frame = cv2.rectangle(frame, (390,1), (485,65), colors[2], -1)
+    frame = cv2.rectangle(frame, (505,1), (600,65), colors[3], -1)
+    cv2.putText(frame, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+    Mask = cv2.inRange(hsv, Lower_hsv, Upper_hsv)
+    Mask = cv2.erode(Mask, kernel, iterations=1)
+    Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
+    Mask = cv2.dilate(Mask, kernel, iterations=1)
+    cnts,_ = cv2.findContours(Mask.copy(), cv2.RETR_EXTERNAL,
+       cv2.CHAIN_APPROX_SIMPLE)
+    center = None
+    # Ifthe contours are formed
+    if len(cnts) > 0:
+    # sorting the contours to find biggest contour
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        # Get the radius of the enclosing circle around the found contour
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        # Draw the circle around the contour
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        # Calculating the center of the detected contour
+        M = cv2.moments(cnt)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+        #checking if any button above the screen is clicked/cursor hovered to
+        if center[1] <= 65:
+            if 40 <= center[0] <= 140: # Clear Button
+                bpoints = [deque(maxlen=512)]
+                gpoints = [deque(maxlen=512)]
+                rpoints = [deque(maxlen=512)]
+                ypoints = [deque(maxlen=512)]
+                blue_index = 0
+                green_index = 0
+                red_index = 0
+                yellow_index = 0
+                paintWindow[67:,:,:] = 255
+            elif 160 <= center[0] <= 255:
+                    colorIndex = 0 # Blue
+            elif 275 <= center[0] <= 370:
+                    colorIndex = 1 # Green
+            elif 390 <= center[0] <= 485:
+                    colorIndex = 2 # Red
+            elif 505 <= center[0] <= 600:
+                    colorIndex = 3 # Yellow
+        else :
+            if colorIndex == 0:
+                bpoints[blue_index].appendleft(center)
+            elif colorIndex == 1:
+                gpoints[green_index].appendleft(center)
+            elif colorIndex == 2:
+                rpoints[red_index].appendleft(center)
+            elif colorIndex == 3:
+                ypoints[yellow_index].appendleft(center)
+    else:
+        bpoints.append(deque(maxlen=512))
+        blue_index += 1
+        gpoints.append(deque(maxlen=512))
+        green_index += 1
+        rpoints.append(deque(maxlen=512))
+        red_index += 1
+        ypoints.append(deque(maxlen=512))
+        yellow_index += 1
+    points = [bpoints, gpoints, rpoints, ypoints]
+    for i in range(len(points)):
+        for j in range(len(points[i])):
+            for k in range(1, len(points[i][j])):
+                if points[i][j][k - 1] is None or points[i][j][k] is None:
+                    continue
+                cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], 2)
+                
+    if cv2.waitKey(1) & 0xFF == ord("s"):
+        file_name = "Trace_Image_" + time.strftime('%Y%m%d%H%M%S') + ".jpg"
+        cv2.imwrite(file_name, paintWindow)
+        
+    cv2.imshow("Tracking", frame)
+    cv2.imshow("Paint", paintWindow)
+    cv2.imshow("mask",Mask)
+    
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+# Release the camera and all resources
+cap.release()
+cv2.destroyAllWindows()
